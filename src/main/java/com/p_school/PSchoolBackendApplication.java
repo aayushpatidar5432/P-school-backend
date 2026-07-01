4c018552295e6a787240d1acce99ee8ab3ca21c5
cochingWork <aayushpatidar5432@gmail.com>
2026-07-01 16:01:28 +0530

    Initial commit

---
+++ src/main/java/com/p_school/PSchoolBackendApplication.java
@@ -0,0 +1,13 @@
+package com.p_school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PSchoolBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PSchoolBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/p_school/PSchoolBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p_school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PSchoolBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
